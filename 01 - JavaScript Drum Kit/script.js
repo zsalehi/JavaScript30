ea2b92102cb61eb1f062bd8cf58d52a9528924fc
Zaigham Salehi <zsalehi313@gmail.com>
2022-03-08 21:07:39 -0500

    Complete JavaScript 30 - 1

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,9 +1,24 @@
-window.addEventListener('keydown', (e) => {
+
+function playSound (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return; // stop from the function from running all together
     audio.currentTime = 0; // rewind the currentTime to 0
     audio.play();
-    key.classList.toggle('playing');
+    key.classList.add('playing');
+    
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== "transform") return; // skip it if its not a transform
+    this.classList.remove('playing');
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach( key => {
+    key.addEventListener('transitionend', removeTransition)
 });
+window.addEventListener('keydown', playSound);
+
 
